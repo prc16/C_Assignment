32408bd63ae16d5aed6240e2404279435ccaa61d
prc16 <prc16@protonmail.com>
2021-12-10 22:12:40 +0530

    Update 06.c

--- C_Assignment_6/06.c
+++ C_Assignment_6/06.c
@@ -15,20 +15,23 @@ void main()
             a[i][j] = j+1+i*3;
 
     for(i=0;i<3;i++)
+    {
         for(j=0;j<3;j++)
-            printf("a[%d][%d] = %d\n",i,j,a[i][j]);
-
+            printf("%d",a[i][j]);
+        printf("\n");
+    }
     getch();
 }
 
 /*----------------------------------------
 Output :
-Enter number of elements in an array : 5
-Enter the value of a[0] : 4
-Enter the value of a[1] : 7
-Enter the value of a[2] : 23
-Enter the value of a[3] : 0
-Enter the value of a[4] : -7
-Smallest = -7
-Largest = 23
+a[0][0] = 1
+a[0][1] = 2
+a[0][2] = 3
+a[1][0] = 4
+a[1][1] = 5
+a[1][2] = 6
+a[2][0] = 7
+a[2][1] = 8
+a[2][2] = 9
 ----------------------------------------*/ 
