010cb24012c556e9efb938815527acbe2f10b800
prc16 <prc16@protonmail.com>
2021-12-10 17:46:13 +0530

    Create 01.c

---
+++ C_Assignment_5/01.c
@@ -0,0 +1,20 @@
+/*
+    Program : Print the following pattern
+    Author  :
+    Date    :
+*/
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n,i,j;
+    printf("Enter the calu of N : ");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        for (j=1;j<i;j++)
+            printf("*");
+        printf("\n");
+    }
+    getch();
+}
