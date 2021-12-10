072e74578edf51ab8024c1a7409c83f7ad551a83
prc16 <prc16@protonmail.com>
2021-12-10 19:11:49 +0530

    Create 04.c

---
+++ C_Assignment_5/04.c
@@ -0,0 +1,33 @@
+/*
+    Program : Print the following pattern
+    Author  :
+    Date    :
+*/
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n,i,j;
+    clrscr();
+    printf("Enter the value of N : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<i;j++)
+            printf(" ");
+        for(j=n;j>=i;j--)
+            printf("*");
+        printf("\n");
+    }
+    getch();
+}
+
+/*----------------------------------------
+Output :
+Enter the value of N : 5
+*****
+ ****
+  ***
+   **
+    *
+----------------------------------------*/
