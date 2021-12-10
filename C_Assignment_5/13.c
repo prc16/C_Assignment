86f6f700c81020249f4b983f356594c6f5aa11f5
prc16 <prc16@protonmail.com>
2021-12-10 19:59:45 +0530

    Create 13.c

---
+++ C_Assignment_5/13.c
@@ -0,0 +1,50 @@
+/*
+    Program : Print the following pattern
+    Author  :
+    Date    :
+*/
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n,i,j;
+    clrscr();
+    printf("Enter the value of N : ");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+            printf("*");
+        for(j=1;j<=2*(n-i);j++)
+            printf(" ");
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    for(i=2;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+            printf("*");
+        for(j=1;j<=2*(n-i);j++)
+            printf(" ");
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    getch();
+}
+
+/*----------------------------------------
+Output :
+Enter the value of N : 5
+*****
+****
+***
+**
+*
+*
+**
+***
+****
+*****
+----------------------------------------*/
