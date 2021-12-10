a0d337cec682a0edcaa1fadc00a5fed156f47883
prc16 <prc16@protonmail.com>
2021-12-10 17:49:14 +0530

    Update 01.c

--- C_Assignment_5/01.c
+++ C_Assignment_5/01.c
@@ -10,9 +10,9 @@ void main()
     int n,i,j;
     printf("Enter the calu of N : ");
     scanf("%d",&n);
-    for (i=1;i<=n;i++)
+    for(i=1;i<=n;i++)
     {
-        for (j=1;j<i;j++)
+        for(j=1;j<i;j++)
             printf("*");
         printf("\n");
     }
