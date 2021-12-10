d7e45dbb76d21fa113852d4dcc14adafe7b93a25
prc16 <prc16@protonmail.com>
2021-12-10 21:09:02 +0530

    Update 04.c

--- C_Assignment_6/04.c
+++ C_Assignment_6/04.c
@@ -17,21 +17,18 @@ void main()
     }
 
     printf("Search for : ");
-    scanf("%d",&n);
+    scanf("%d",&search);
 
     for(i=0;i<10;i++)
     {
-        if(a[i]==n)
+        if(a[i]==search)
         {
             flag = 1;
-            printf("Element found at a[%d] = %d.",i,a[i]);
+            printf("Element found at a[%d] = %d.\n",i,a[i]);
         }
     }
     if(flag==0)
-    printf("Element not found.");
-
-    avg=sum/5;
-    printf("sum = %.2f avg = %.2f\n",sum,avg);
+    printf("Element not found.\n");
     getch();
 }
 
@@ -40,7 +37,15 @@ Output :
 Enter the value of a[0] : 1
 Enter the value of a[1] : 2
 Enter the value of a[2] : 3
-Enter the value of a[3] : 4
-Enter the value of a[4] : 5
-sum = 15.00 avg = 3.00
+Enter the value of a[3] : 44
+Enter the value of a[4] : 44
+Enter the value of a[5] : 5
+Enter the value of a[6] : 5
+Enter the value of a[7] : 44
+Enter the value of a[8] : 5
+Enter the value of a[9] : 6
+Search for : 44
+Element found at a[3] = 44.
+Element found at a[4] = 44.
+Element found at a[7] = 44.
 ----------------------------------------*/ 
