9e3be12e7604d6d6e6449bfef9737fe861669c01
prc16 <prc16@protonmail.com>
2021-12-10 18:23:45 +0530

    Create 02.c

---
+++ C_Assignment_5/02.c
@@ -0,0 +1,39 @@
+/*
+    Program : 
+    Autor   :
+    Date    :
+*/
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n, i, j;
+    clrscr();
+    printf("Enter the value of N : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    for(i=n-1;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+            printf("*");
+        printf("\n");
+    }
+    getch();
+
+}
+
+/*
+    Output :
+    Enter the value of N : 5
+    *
+    **
+    ***
+    ****
+    *****
+*/
